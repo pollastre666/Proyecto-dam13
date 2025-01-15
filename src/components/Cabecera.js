241aef7ac4b33d428d63d95de0832650d45e3120
pollastre666 <alainsingla8@gmail.com>
2025-01-15 15:49:40 +0100

    segon

--- src/components/Cabecera.js
+++ src/components/Cabecera.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import '../estils/Cabecera.css';
 import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png'; 
 
 const Cabecera = () => {
     return (
         <header className="cabecera">
-            <h1>CPU Finder</h1>
+            <div className="logo-container">
+                <img src={logo} alt="CPU Finder Logo" className="logo" />
+                <h1>CPU Finder</h1>
+            </div>
             <nav>
                 <ul>
-                <li><Link to="/">Inicio</Link></li>
-                <li><Link to="/quienes-somos">Quiénes somos</Link></li>
-                <li><Link to="/contacto">Contacto</Link></li>
+                    <li><Link to="/">Inicio</Link></li>
+                    <li><Link to="/quienes-somos">Quiénes somos</Link></li>
+                    <li><Link to="/contacto">Contacto</Link></li>
                 </ul>
             </nav>
             <div className="auth">
